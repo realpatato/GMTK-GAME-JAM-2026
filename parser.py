28d701692890ebff39147c777e85c1015febce95
realpatato <jason.t.richardii@gmail.com>
2026-07-22 15:28:41 -0400

    scaling boi

--- parser.py
+++ parser.py
@@ -1,9 +1,12 @@
 class Sprite():
-    def __init__(self, x, y, w, h):
-        self.rects = [(x, y, w, h)]
+    def __init__(self, x, y, w, h, sf):
+        self.rects = [(x * sf, y * sf, w * sf, h * sf)]
+
+    def rect(self):
+        return self.rects[0]
 
 class AnimatedSprite(Sprite):
-    def __init__(self, x, y, w, h, ct):
+    def __init__(self, x, y, w, h, sf, ct):
         super().__init__(x, y, w, h)
         for i in range(ct - 1):
-            self.rects.append((x + w * i, y + h * i, w, h))
+            self.rects.append((x + w * sf * i, y + h * sf * i, w * sf, h *sf))
--- states/play_state.py
+++ states/play_state.py
@@ -3,12 +3,19 @@ import pygame
 import parser
 
 class PlayState(BaseState):
+    def __init__(self):
+        super().__init__()
+        self.spritesheet = pygame.image.load("assets/Spritesheet.png").convert_alpha()
+        self.sssf = 2
+        self.spritesheet = pygame.transform.scale_by(self.spritesheet, self.sssf)
+
     def enter(self, persistent_data):
         super().enter(persistent_data)
 
     def draw(self, screen):
         screen.fill((0, 255, 0))
-        screen.blit()
+        bg = parser.Sprite(0, 0, 16, 16, self.ssf)
+        screen.blit(self.spritesheet, (0, 0), bg.rect())
 
     def handle_event(self, event):
         if event.type == pygame.KEYDOWN:
