61364fc2d047d930c0db5f946bf8d773017138e7
timtrippjr <trytimatops2@gmail.com>
2026-07-22 18:36:11 -0400

    basic file save

--- level.py
+++ level.py
@@ -1,3 +1,46 @@
+import pygame
+import json
+from constants import *
+from pathlib import Path
+
+
 class Level():
-    def __init__(self):
-        self.tiles = []
+    def __init__(self, tiles = {}):
+        #give levels names. make button and inputbox so that we can set the name, lvl w/h, and click open file button
+        self.tiles = tiles
+        self.w = 20
+        self.h = 12
+        self.name = 'untitled'
+
+
+    def draw(self, screen, off_x = 0, off_y = 0):
+        for pos, tiletype in self.tiles.items():
+            x, y = [int(x) for x in pos.split(",")]
+
+            #debug
+            if tiletype == "Ground":
+                pygame.draw.rect(
+                    screen, 
+                    (0,0,255), 
+                    (
+                        x * TILE_SIZE + int(off_x), 
+                        y * TILE_SIZE + int(off_y), 
+                        TILE_SIZE, 
+                        TILE_SIZE
+                    )
+                )
+            #screen.blit
+
+    def save(self, filename):
+        dir_path = Path('assets/rooms')
+        dir_path.mkdir(parents=True, exist_ok=True)
+
+        path = f'assets/rooms/{filename}.json'
+
+        with open(path, 'w') as f:
+            json.dump({
+                "name": filename,
+                "size": { "width": self.w, "height": self.h },
+                "tiles": self.tiles
+            }, f, indent=4) 
+        print('saved to '+path)
