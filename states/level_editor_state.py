8c8d51d78b19c665e7ed262e55462176ae4f5287
timtrippjr <trytimatops2@gmail.com>
2026-07-22 15:29:06 -0400

    aa

--- states/level_editor_state.py
+++ states/level_editor_state.py
@@ -31,9 +31,8 @@ class LevelEditorState(BaseState):
             ),
             (0, 0)
         )
-
-
         pygame.draw.rect(screen, (255,255,255), (self.cursor_x * TILE_SIZE, self.cursor_y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+        
     def handle_event(self, event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
