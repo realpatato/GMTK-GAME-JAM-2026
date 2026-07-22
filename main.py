08a3d37c02d29305d1342e43c772cfa97b2f569c
realpatato <jason.t.richardii@gmail.com>
2026-07-22 13:04:51 -0400

    pygame starter code

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World")
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1000, 600))
+
+clock = pygame.time.Clock()
+keep_playing = True
+
+while keep_playing:
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            keep_playing = False
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit()
