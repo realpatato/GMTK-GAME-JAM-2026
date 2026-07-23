7cd3adccaeb5ab9d6bc45959787aaf89a07f688f
timtrippjr <trytimatops2@gmail.com>
2026-07-22 22:10:49 -0400

    level editor boxes!!

--- gui.py
+++ gui.py
@@ -1,20 +1,49 @@
 import pygame
 #for things like text boxes, buttons, the likes
 
-class Button:
-    def __init__(self, x, y, w, h, text):
-        self.rect = pygame.Rect(x,y,w,h)
-
+class Gui:
+    def __init__():
+        pass
+    def handle_event(self, event):
+        pass
     def update(self, dt):
         pass
     def draw(self, screen):
         pass
 
-class InputBox:
-    def __init__(self, x, y, w, h, text):
-        self.rect = pygame.Rect(x,y,w,h)
+class InputBox(Gui):
+    def __init__(self, x, y, w, font, placeholder_text = ''):
+        self.placeholder_text = placeholder_text
+        self.text = ''
+        self.font = font
+        self.placeholder_image = font.render(placeholder_text, True, (160, 160, 160))
+        h = self.placeholder_image.get_height()
+        self.rect = pygame.Rect(x,y,w,h+2)
+
+    def get_value(self):
+        return self.text
 
     def update(self, dt):
         pass
-    def draw(self, screen):
-        pass
+
+    def handle_event(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSPACE:
+                self.text = self.text[:-1]
+                return
+            self.text += event.unicode
+
+    def draw(self, screen, focused):
+        if focused:
+            pygame.draw.rect(screen, (64, 64, 64), self.rect)
+            pygame.draw.rect(screen, (96, 96, 96), self.rect, 1)
+        else:
+            pygame.draw.rect(screen, (32, 32, 32), self.rect)
+            pygame.draw.rect(screen, (48, 48, 48), self.rect, 1)
+        if not self.text:
+            screen.blit(self.placeholder_image, (self.rect.x+2, self.rect.y))
+        else:
+            screen.blit(
+                self.font.render(self.text, True, (255, 255, 255)),
+                (self.rect.x+2, self.rect.y)
+            )
--- states/level_editor_state.py
+++ states/level_editor_state.py
@@ -1,14 +1,22 @@
 from .base_state import BaseState
 from level import Level
 import pygame
+from gui import *
 from constants import *
 
 class LevelEditorState(BaseState):
     def __init__(self):
         super().__init__()
-        self.font = pygame.font.Font('assets/Bomby.ttf', 16)
+        self.font = pygame.font.Font('assets/Bomby.ttf', 12)
 
         self.level = Level()
+
+        self.gui = {
+            "level title": InputBox(30, 1, 60, self.font, "untitled"),
+            "level width": InputBox(128, 1, 22, self.font, "20"),
+            "level height": InputBox(196, 1, 22, self.font, "12"),
+        }
+        self.focused_gui = None
 
         self.tile_type = "Ground"
 
@@ -22,11 +30,30 @@ class LevelEditorState(BaseState):
         super().enter(persistent_data)
 
     def update(self, dt):
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        self.cursor_x, self.cursor_y = [
-            ((mouse_x / SCALE_FACTOR) - self.cam_x) // TILE_SIZE,
-            ((mouse_y / SCALE_FACTOR) - self.cam_y) // TILE_SIZE
-        ]
+
+        if self.focused_gui:
+            self.focused_gui.update(dt)
+        self.level.name = self.gui["level title"].get_value() or "untitled"
+
+        w_value = self.gui["level width"].get_value()
+        if w_value.isdigit(): self.level.w = int(w_value)
+        h_value = self.gui["level height"].get_value()
+        if h_value.isdigit(): self.level.h = int(h_value)
+
+        # place thigns
+        if not self.focused_gui:
+            if (
+                    0 <= self.cursor_x < self.level.w 
+                and 0 <= self.cursor_y < self.level.h
+            ):
+                x, y = int(self.cursor_x), int(self.cursor_y)
+                key = f'{x},{y}'
+                mouse_buttons = pygame.mouse.get_pressed()
+                if mouse_buttons[0]:
+                    self.level.tiles[key] = self.tile_type
+                if mouse_buttons[2]:
+                    if key in self.level.tiles:
+                        del self.level.tiles[key]
 
     def draw(self, screen):
         screen.fill((0, 0, 0))
@@ -43,7 +70,7 @@ class LevelEditorState(BaseState):
         )
 
         #draw levle
-        self.level.draw(screen, self.cam_x, self.cam_y)
+        self.level.draw(screen, math.floor(self.cam_x), math.floor(self.cam_y))
 
         # cursor 
         rect_surface = pygame.Surface((TILE_SIZE, TILE_SIZE), pygame.SRCALPHA)
@@ -51,37 +78,46 @@ class LevelEditorState(BaseState):
         screen.blit(
             rect_surface, 
             (
-                self.cursor_x * TILE_SIZE + self.cam_x, 
-                self.cursor_y * TILE_SIZE + self.cam_y
+                self.cursor_x * TILE_SIZE + math.floor(self.cam_x), 
+                self.cursor_y * TILE_SIZE + math.floor(self.cam_y)
             )
         )
-
-    
-        # place thigns
-        if 0 <= self.cursor_x < self.level.w and 0 <= self.cursor_y < self.level.h:
-            x, y = int(self.cursor_x), int(self.cursor_y)
-            key = f'{x},{y}'
-            mouse_buttons = pygame.mouse.get_pressed()
-            if mouse_buttons[0]:
-                self.level.tiles[key] = self.tile_type
-            if mouse_buttons[2]:
-                if key in self.level.tiles:
-                    del self.level.tiles[key]
 
 
 
         #info text
-        screen.blit(
-            self.font.render(
-                f'{self.level.name} (width: {self.level.w}, height: {self.level.h})', 
-                True, 
-                (255, 255, 255)
-            ),
-            (0, 0)
-        )
+        screen.blit(self.font.render("name:", True, (255, 255, 255)),(0, 1))
+        screen.blit(self.font.render("width:", True, (255, 255, 255)),(90, 1))
+        screen.blit(self.font.render("height:", True, (255, 255, 255)),(150, 1))
+
+        for elmnt in self.gui.values():
+            elmnt.draw(screen, self.focused_gui == elmnt)
 
 
     def handle_event(self, event):
+        if event.type == pygame.MOUSEMOTION:
+            mouse_x, mouse_y = event.pos
+            mouse_x /= SCALE_FACTOR
+            mouse_y /= SCALE_FACTOR
+            self.cursor_x, self.cursor_y = [
+                (mouse_x - self.cam_x) // TILE_SIZE,
+                (mouse_y - self.cam_y) // TILE_SIZE
+            ]
+            self.focused_gui = next(
+                (
+                    elmnt 
+                    for elmnt in self.gui.values()
+                        if elmnt.rect.collidepoint((mouse_x, mouse_y))
+                ), 
+                None
+            )
+
+        #GUI HANDLE EVENT
+        if self.focused_gui:
+            self.focused_gui.handle_event(event)
+            return
+
+        #STATE EVENTS
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 self.next_state = "play_state"
